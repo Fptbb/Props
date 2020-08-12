283caf8f2417f01d0a70f130553affeabdce487c
Lucas Oliveira <lucasmateusoliveira0@gmail.com>
2020-08-11 21:56:36 -0300

    update DevSetup.ps1

--- shell/DevSetup.ps1
+++ shell/DevSetup.ps1
@@ -1,6 +1,7 @@
 #By Fptbb
 #Configura VSCode, Git e Nodejs (Latest) na maquina e seta os Paths
-$arch = "64" #64x ou 86x
+$arch = "64" #64x ou 86x Padrão: 86x(32x)
+$NodeVersion = "lts" #lts ou latest Padrão: latest
 
 function download {
     param (
@@ -27,19 +28,12 @@ function download {
 	[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::User);
 }
 
-#Set download links that is stable (Update)
-
-#Vai baixar a ultima versão do git
-$PortableGit = "https://fptbb.com/program/latest/PortableGit?arch=$arch"
-#Vai baixar a ultima versão do node latest, adicione &version=lts caso queira a Long Term Support
-$NodeFile = "https://fptbb.com/program/latest/Node?arch=$arch"
-
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 download -RemoteFile "https://go.microsoft.com/fwlink/?Linkid=850641" -DownloadFile $env:Temp"\vscode.zip" -DoExtractFile $true -AddedPath $env:LOCALAPPDATA"\VsCode"
-download -RemoteFile $NodeFile -DownloadFile $env:Temp'\node.zip' -DoExtractFile $true -AddedPath $env:LOCALAPPDATA"\Node"
+download -RemoteFile "https://fptbb.com/program/latest/Node?arch=$arch&version=$NodeVersion" -DownloadFile $env:Temp'\node.zip' -DoExtractFile $true -AddedPath $env:LOCALAPPDATA"\Node"
 Move-Item -Path $env:LOCALAPPDATA"\Node\node" -Destination $env:LOCALAPPDATA"\Node1"
 Remove-item -Path $env:LOCALAPPDATA"\Node"
 Rename-item -Path $env:LOCALAPPDATA"\Node1" -NewName $env:LOCALAPPDATA"\Node"
 npm install yarn -g
-download -RemoteFile $PortableGit -DownloadFile $env:TEMP'\PortableGit.exe' -DoExtractFile $false -AddedPath $env:LOCALAPPDATA'\Git' -Debug
+download -RemoteFile "https://fptbb.com/program/latest/PortableGit?arch=$arch" -DownloadFile $env:TEMP'\PortableGit.exe' -DoExtractFile $false -AddedPath $env:LOCALAPPDATA'\Git' -Debug
 & $env:TEMP'\PortableGit.exe' -o $env:LOCALAPPDATA'\Git' -y
