ea7e66440e1e6f2dc132f5ec2bec436204e80982
Lucas Oliveira <lucasmateusoliveira0@gmail.com>
2022-01-29 12:14:26 -0300

    Update vr.js

--- vr.js
+++ vr.js
@@ -54,3 +54,28 @@ if(Storage && window.localStorage && window.localStorage instanceof Storage) {
 else {
     //console.warn("Local storage === false");
 }
+// Alternative RR
+var pattern = ['r', 'i', 'c', 'k'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+		window.location.href = V
+	}
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
